42f56101f2c934f4e4ec35ef1f7a53aa34573e30
Juin Chiu <davidjuin0519@gmail.com>
2016-10-08 21:38:18 +0800

    Implement compose

---
+++ lib/rubidux/util.rb
@@ -0,0 +1,9 @@
+module Rebidux
+  class Util
+    def self.compose(*funcs)
+      last = funcs[funcs.size-1]
+      rest = funcs[0..funcs.size-2].reverse
+      -> (*args) { rest.reduce(last(*args)) { |composed, f| f(composed) } }
+    end
+  end
+end
