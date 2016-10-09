9af8701fe76015c9142e503eb174f033568467d7
Juin Chiu <davidjuin0519@gmail.com>
2016-10-09 17:25:53 +0800

    Use conditional assignment

--- lib/rubidux/reducer.rb
+++ lib/rubidux/reducer.rb
@@ -2,7 +2,7 @@ module Rubidux
   class Reducer
     def self.combine(**reducers)
       -> (state, action) {
-        state = {} unless state
+        state ||= {}
         Hash[reducers.map { |key, reducer| [key, reducer.(state[key], action)] }]
       }
     end
