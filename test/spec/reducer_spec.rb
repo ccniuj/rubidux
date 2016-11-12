f4ddcdfc5f1d8fdcb67446562b6f9cd67811ae5a
Juin Chiu <davidjuin0519@gmail.com>
2016-11-12 14:28:57 +0800

    Curry method combine

--- test/spec/reducer_spec.rb
+++ test/spec/reducer_spec.rb
@@ -57,16 +57,16 @@ describe Rubidux::Reducer do
 
   describe "combine" do
     it "returns a composite reducer that maps the state keys to given reducers" do
-      rc = Rubidux::Reducer.combine(r1: @r_ab, r2: @r_cd)
+      rc = Rubidux::Reducer.combine.(r1: @r_ab, r2: @r_cd)
       new_state = rc.({}, {})
 
       new_state.keys.must_equal [:r1, :r2]
     end
 
     it "returns a composite reducer of some other composite reducers" do
-      rc1 = Rubidux::Reducer.combine(r1: @r_ab, r2: @r_cd)
-      rc2 = Rubidux::Reducer.combine(r3: @r_ef, r4: @r_gh)
-      rc = Rubidux::Reducer.combine(rc1: rc1, rc2: rc2)
+      rc1 = Rubidux::Reducer.combine.(r1: @r_ab, r2: @r_cd)
+      rc2 = Rubidux::Reducer.combine.(r3: @r_ef, r4: @r_gh)
+      rc = Rubidux::Reducer.combine.(rc1: rc1, rc2: rc2)
       new_state = rc.({}, {})
 
       new_state.keys.must_equal [:rc1, :rc2]
@@ -75,7 +75,7 @@ describe Rubidux::Reducer do
     end
 
     it "ignores all keys whose value are not a function" do
-      rc = Rubidux::Reducer.combine(r1: @r_ab, r2: "foo", r3: {foo: "bar"} )
+      rc = Rubidux::Reducer.combine.(r1: @r_ab, r2: "foo", r3: {foo: "bar"} )
       new_state = rc.({}, {})
 
       new_state.keys.must_equal [:r1]
