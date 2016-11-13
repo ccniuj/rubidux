73c9a84b4395bc540b6fe12d0715bae2d1d129ae
Juin Chiu <davidjuin0519@gmail.com>
2016-11-14 00:20:36 +0800

    Refactor store

--- lib/rubidux/store.rb
+++ lib/rubidux/store.rb
@@ -1,26 +1,38 @@
 module Rubidux
   class Store
     attr_reader :state
-    attr_reader :reducer
-    attr_reader :listeners
     attr_reader :dispatch
     attr_reader :subscribe
+    attr_reader :listeners
+    attr_accessor :reducer
 
-    def initialize(reducer, preload_state, enhancer = nil)
+    INITIALIZE = :INITIALIZE
+
+    def initialize(reducer:, preloaded_state: {}, enhancer: nil)
+      raise ArgumentError.new("Expect preloaded state to be a Hash.") unless preloaded_state.is_a? Hash
       raise ArgumentError.new("Expect reducer to be a Proc.") unless reducer.is_a? Proc
-      @state = preload_state || {}
-      @listeners = []
+      raise ArgumentError.new("Expect enhancer to be a Proc.") unless (!enhancer || enhancer.is_a?(Proc))
+
+      @state = {}
       @reducer = reducer
-      @dispatch = _dispatch
+      @listeners = []
       @subscribe = _subscribe
 
-      @dispatch = enhancer.(-> { @state }, @dispatch) if enhancer
+      @dispatch = enhancer ? enhancer.(-> { @state }, _dispatch) : _dispatch
+      @dispatch.({ type: INITIALIZE })
+
+      @state = @state.merge preloaded_state
     end
 
     private
 
+    attr_writer :state
+    attr_writer :dispatch
+    attr_writer :subscribe
+    attr_writer :listeners
+
     def _dispatch
-      -> (action) {
+      -> action {
         raise ArgumentError.new("Expect action to have key 'type'.") unless action[:type]
         @state = @reducer.(@state, action)
         @listeners.each(&:call)
@@ -29,19 +41,11 @@ module Rubidux
     end
 
     def _subscribe
-      -> (listener) {
+      -> listener {
         raise ArgumentError.new("Expect listener to be a Proc.") unless listener.is_a? Proc
         @listeners.push listener
         -> { @listeners.delete listener }
       }
     end
   end
-
-  private
-
-  attr_writer :state
-  attr_writer :reducer
-  attr_writer :listeners
-  attr_writer :dispatch
-  attr_writer :subscribe
 end
